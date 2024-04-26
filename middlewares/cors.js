586f2ce9da3733669c518bfb63b22a27df031040
S3gaM3 <120776048+S3gaM3@users.noreply.github.com>
2024-04-26 19:59:01 +0300

    ver 1.0

---
+++ middlewares/cors.js
@@ -0,0 +1,16 @@
+const allowedCors = [
+    'https://practicum.yandex.ru',
+    'http://practicum.yandex.ru',
+    'http://localhost:3000'
+  ];
+  
+  function cors(req, res, next) {
+    const { origin } = req.headers;
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+    next();
+  }
+  
+  module.exports = cors;
+  
