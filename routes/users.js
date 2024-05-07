a3fa6582711b3779d4939c8e1f5b96dc66576fc6
S3gaM3 <120776048+S3gaM3@users.noreply.github.com>
2024-05-07 20:12:48 +0300

    .

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 // routes/users.js
 const express = require('express');
 const usersRouter = express.Router();
-const { findAllUsers, createUser, findUserById, deleteUser, checkEmptyNameAndEmailAndPassword, checkEmptyNameAndEmail, checkIsUserExists } = require('../middlewares/users');
-const { sendAllUsers, sendUserCreated, sendUserById, sendUserDeleted } = require('../controllers/users');
-const updateUser = require('../middlewares/users').updateUser;
-const sendUserUpdated = require('../controllers/users').sendUserUpdated;
+const { findAllUsers, createUser, findUserById, deleteUser, checkEmptyNameAndEmailAndPassword, checkEmptyNameAndEmail, checkIsUserExists, updateUser } = require('../middlewares/users');
+const { sendAllUsers, sendUserCreated, sendUserById, sendUserDeleted, sendUserUpdated } = require('../controllers/users');
+
 
 
 usersRouter.get('/users', findAllUsers, sendAllUsers);
@@ -12,6 +11,5 @@ usersRouter.post('/users', checkIsUserExists, checkEmptyNameAndEmailAndPassword,
 usersRouter.get('/users/:id', findUserById, sendUserById);
 usersRouter.put('/users/:id', checkEmptyNameAndEmail, updateUser, sendUserUpdated);
 usersRouter.delete('/users/:id', deleteUser, sendUserDeleted);
-
 
 module.exports = usersRouter;
