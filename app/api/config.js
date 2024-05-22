3ac938cbd827343f03957f21a60f1d3989fb88bd
S3gaM3 <kiborgik87@gmail.com>
2024-05-22 21:56:24 +0300

    del front

--- app/api/config.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = 'https://s3ga.nomoredomainswork.ru/api'
-
-export const endpoints = {
-  games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/local`,
-  me: `${BASE_URL}/users/me`,
-}
